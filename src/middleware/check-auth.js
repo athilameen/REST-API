11ac468e3615eedb56f057e3fa8968f329f9bed8
Athil Ameen <athilammeen@gmail.com>
2024-08-25 20:47:51 +0530

    CheckAuth Log 008

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -4,28 +4,13 @@ const {redisClient, redisKeys } = require('../connect/connectRedis');
 module.exports = async (req, res, next) => {
     try {
 
-        return res.status(200).json({ 
-            //req: req, 
-            myheaders: req.headers, 
-            mymessage: req.headers.authorization
-        });
-
-        /*
         const token = (req.headers.authorization) ? req.headers.authorization.split(" ")[1] : "";   
-
-        console.log('State 1');
-        
         
         if (!token || token == null || token === "null") {
-            console.log('State 2');
-            return res.status(401).json({ message: "Unauthorized State 1", token});
+            return res.status(401).json({ message: "Unauthorized"});
         } else {
 
-            console.log('State 3');
-
             jwt.verify(token, process.env.JWT_SECRET, async (err, verifyTokenRes) => {
-
-                console.log('State 4');
 
                 if(err){
                     return res.status(403).json({ message: "Invalid token info"})
@@ -35,8 +20,7 @@ module.exports = async (req, res, next) => {
                 const user = await redisClient.get(`${redisKeys.USER_STATE}:${sessionId}`);
                 
                 if (!user){
-                    console.log('State 5');
-                    return res.status(401).json({ message: "Unauthorized State 2"});
+                    return res.status(401).json({ message: "Unauthorized"});
                 }
 
                 req.userData = JSON.parse(user);
@@ -44,15 +28,12 @@ module.exports = async (req, res, next) => {
 
             });
             
-        }*/
+        }
        
     } catch (error) {
         console.log(error);
         return res.status(401).json({
             message:  'Authentication failed.',
-             //req: req, 
-             myheaders: req.headers, 
-             mymessage: req.headers.authorization
         });
     }
 };

8d927f7dfc0ca4cac91f7b7a2d0d748d2b8b539d
Athil Ameen <athilammeen@gmail.com>
2024-08-25 19:59:35 +0530

    CheckAuth Log 001

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -5,12 +5,20 @@ module.exports = async (req, res, next) => {
     try {
 
         const token = (req.headers.authorization) ? req.headers.authorization.split(" ")[1] : "";   
+
+        console.log('State 1');
+        
         
         if (!token || token == null || token === "null") {
+            console.log('State 2');
             return res.status(401).json({ message: "Unauthorized"});
         } else {
 
+            console.log('State 3');
+
             jwt.verify(token, process.env.JWT_SECRET, async (err, verifyTokenRes) => {
+
+                console.log('State 4');
 
                 if(err){
                     return res.status(403).json({ message: "Invalid token info"})
@@ -20,6 +28,7 @@ module.exports = async (req, res, next) => {
                 const user = await redisClient.get(`${redisKeys.USER_STATE}:${sessionId}`);
                 
                 if (!user){
+                    console.log('State 5');
                     return res.status(401).json({ message: "Unauthorized"});
                 }
 

b409d0360f5b5a17dd5d6f1eb9783d52fa8e711f
Athil Ameen <athilammeen@gmail.com>
2024-08-26 22:12:29 +0530

    Redis expiresIn time limit set

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -125,7 +125,8 @@ exports.userLogin = async (req, res) => {
         refreshToken
       }
 
-      const expiresIn = 60 * 60 * 24 * 30; // 30 days
+      //const expiresIn = 60 * 60 * 24 * 30; // 30 days
+      const expiresIn = 60 * 3; // 2 min
       await redisClient.set(`${redisKeys.USER_STATE}:${sessionId}`, JSON.stringify(userInfo), { EX: expiresIn });
       return res.success(userData, 'Login Success');
 
